b5650fe94989d78d42d4d91b650b1238d517bb32
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-04-20 00:29:01 +0300

    Kotlin

---
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlintuts
+
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
+fun main(){
+    var ageGrup: Int = 5
+    println(ageGrup)
+}
