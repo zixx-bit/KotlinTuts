a285c5fcdc70c3b6b515b2d80ed5c48876890156
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-02 23:49:49 +0300

    addition

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -1,14 +1,14 @@
-//package com.example.kotlintuts
-//
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
+package com.example.kotlintuts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
 fun main(){
   val myArray = arrayOf(4,5,6,1,3)
     var max = myArray[0]
