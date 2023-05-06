9e2213ec8c9867e9a8f222880c467d2b47da2bf7
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-06 13:42:32 +0300

    create function on one line

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -4,10 +4,12 @@ fun main(){
 
    val pow = printPow(3,5)
     println("3 to the power of 5 is $pow")
-
-
+    multiply(3,5)
+    println(multiply(3, 5))
 }
 
+//one line function
+fun multiply(a:Int , b:Int) = a * b
 fun printPow(base: Int, exponent: Int): Int{
     var result = 1
     for (i in 1..exponent){
