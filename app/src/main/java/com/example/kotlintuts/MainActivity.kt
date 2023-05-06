c6933e57b43aa8740db37332ff1df5ac526a3f66
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-06 13:35:40 +0300

    functions base to power add return

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -1,17 +1,19 @@
 
 
 fun main(){
-    printPow(2,2)
-    printPow(100,4)
+
+   val pow = printPow(3,5)
+    println("3 to the power of 5 is $pow")
 
 
 }
 
-fun printPow(base: Int, exponent: Int){
+fun printPow(base: Int, exponent: Int): Int{
     var result = 1
     for (i in 1..exponent){
         result = result * base
     }
-    println("$base to power $exponent is $result")
+//    println("$base to power $exponent is $result")
+    return result
 
 }
