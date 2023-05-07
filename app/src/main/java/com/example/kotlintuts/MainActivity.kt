3f4ce6051de3e8bf4fd7f04418dd7bd40bfbcb47
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-07 23:50:48 +0300

    extension functions

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -3,6 +3,17 @@
 fun main(){
     println("Please enter a number")
     val input = readLine()?.toInt()
+
+    if (input != null){
+
+    }
 }
 
-fun
+fun Int.isPrime(): Boolean{
+    for (i in 2 until this-1){
+        if (this % 1 == 0){
+            return false
+        }
+    }
+    return true
+}
