9a5114060363cc8b745bbc28054756dfe0e6e29a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-07 23:46:16 +0300

    extension functions

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -1,22 +1,8 @@
 
 
 fun main(){
-    var numbers = readLine()?.toInt()
-   var result = getMax()
-    println(result)
-  searchFor("how to become a good progmmer")
-    searchFor("how to become a good programmer", "bing")
+    println("Please enter a number")
+    val input = readLine()?.toInt()
+}
 
-}
-fun searchFor(search: String, searchEngine: String = "Google"){
-    println("seraching for '$search' on $searchEngine")
-}
-fun getMax(vararg numbers: Int): Int{
-    var max = numbers[0]
-    for (i in numbers){
-        if (i > max){
-            max = i
-        }
-    }
-    return max
-}
+fun
