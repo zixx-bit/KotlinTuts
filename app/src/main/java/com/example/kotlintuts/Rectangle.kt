ffe43137b50605b53136cf8e21cf64fb471271f9
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-08 20:21:32 +0300

    Classes

--- app/src/main/java/com/example/kotlintuts/Rectangle.kt
+++ app/src/main/java/com/example/kotlintuts/Rectangle.kt
@@ -2,7 +2,13 @@ package com.example.kotlintuts
 
 class Rectangle(val a: Double,val b: Double){
     init {
-        var result = a*b
-        println("Reactangle created with a= $a and b = $b is $result")
+
+        println("Reactangle created with a= $a and b = $b is ")
     }
+
+    fun area() = a * b
+
+    fun perimeter() = 2 * a +2*b
+
+    fun isSquare() = a==b
 }
