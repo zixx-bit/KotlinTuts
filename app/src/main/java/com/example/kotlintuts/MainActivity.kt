ec6ebb1307d44dd1e48f8863a0eefdaf6ff4578d
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-08 20:21:49 +0300

    Classes

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -2,4 +2,8 @@ import com.example.kotlintuts.Rectangle
 
 fun main(){
     val myRect = Rectangle(10.0, 20.0 )
+    println("area is ${myRect.area()}")
+    println("perimeter is ${myRect.perimeter()}")
+    println("is square? is ${myRect.isSquare()}")
+
 }
