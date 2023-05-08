37e463bd19241fd9e9439f3f264f27a5710e3347
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-08 22:25:54 +0300

    Classes

--- app/src/main/java/com/example/kotlintuts/Triangle.kt
+++ app/src/main/java/com/example/kotlintuts/Triangle.kt
@@ -1,9 +1,12 @@
 package com.example.kotlintuts
 
+import kotlin.math.sqrt
+
 class Triangle(val a:Double, val b:Double, val c:Double) {
     init {
         println("The area of triangle is ${area()}")
         println("The perimeter of triangle is ${perimeter()}")
+        println(sqrt(9.0 ))
     }
 
 
