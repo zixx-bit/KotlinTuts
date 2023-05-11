44e7a63d9ffb4c0741d5a7aa22e26b7ebcb4eea1
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-11 22:18:27 +0300

    Classes visibility modifiers

--- app/src/main/java/com/example/kotlintuts/Shape.kt
+++ app/src/main/java/com/example/kotlintuts/Shape.kt
@@ -1,10 +1,13 @@
 package com.example.kotlintuts
-
-open class Shape(var name: String) {
+ abstract class Shape(var name: String) {
     init {
         println("I am a super class")
     }
     fun changeName(newName : String ){
         name = newName
     }
+
+     abstract fun area()
+
+     abstract fun perimeter()
 }
