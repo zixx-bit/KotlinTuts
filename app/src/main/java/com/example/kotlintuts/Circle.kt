07a3377349c16d0c62ae3603965f010c1ab563b1
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-12 11:07:00 +0300

    abstract classes and constructor

--- app/src/main/java/com/example/kotlintuts/Circle.kt
+++ app/src/main/java/com/example/kotlintuts/Circle.kt
@@ -1,7 +1,6 @@
 package com.example.kotlintuts
 
 class Circle(val radius: Double): Shape("rose") {
-  private val pi=3.142
     init {
         println("circle created with radius = $radius")
     }
@@ -12,6 +11,6 @@ class Circle(val radius: Double): Shape("rose") {
 
     }
 
-    override fun area() = radius * radius * pi
-    override fun perimeter() = 2 * radius * pi
+    override fun area() = radius * radius * ImportantNumbers.PI
+    override fun perimeter() = 2 * radius * ImportantNumbers.PI
 }
--- app/src/main/java/com/example/kotlintuts/ImportantNumbers.kt
+++ app/src/main/java/com/example/kotlintuts/ImportantNumbers.kt
@@ -1,4 +1,5 @@
 package com.example.kotlintuts
 
 object ImportantNumbers {
+    val PI = 3.142
 }
