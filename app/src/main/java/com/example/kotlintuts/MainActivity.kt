70f237761e45f43f324b80a69d480d6f3020bb89
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-05-15 11:45:26 +0300

    exceptions

--- app/src/main/java/com/example/kotlintuts/MainActivity.kt
+++ app/src/main/java/com/example/kotlintuts/MainActivity.kt
@@ -3,25 +3,37 @@ import com.example.kotlintuts.Rectangle
 import com.example.kotlintuts.Triangle
 
 fun main(){
-    val myRect = Rectangle(10.0, 20.0 )
-    println("area is ${myRect.area()}")
-    println("perimeter is ${myRect.perimeter()}")
-    println("is square? is ${myRect.isSquare()}")
+//    val myRect = Rectangle(10.0, 20.0 )
+//    println("area is ${myRect.area()}")
+//    println("perimeter is ${myRect.perimeter()}")
+//    println("is square? is ${myRect.isSquare()}")
+//
+//    val myCircle = Circle(7.0)
+//    myCircle.changeName("pter")
+//    println("area of circle is ${myCircle.area()} and ${myCircle.name}")
+//    println("perimeter of circle is ${myCircle.perimeter()}")
+//
+//    val myTriangle = Triangle(2.0,4.0,6.0)
+//    val myc = Circle
+//    val c = myc.randomCircle()
+//
+//    val list = listOf(1,2,3,4,5)
+//    val array = arrayOf(1,2,3,4,5)
+//
+//    println("List: $list")
+//    printAlternately(array)
 
-    val myCircle = Circle(7.0)
-    myCircle.changeName("pter")
-    println("area of circle is ${myCircle.area()} and ${myCircle.name}")
-    println("perimeter of circle is ${myCircle.perimeter()}")
+    println("Please enter a number")
+    val input = try {
+        readln()?.toInt()
 
-    val myTriangle = Triangle(2.0,4.0,6.0)
-    val myc = Circle
-    val c = myc.randomCircle()
+    }catch (e: NumberFormatException){
+        0
+    }finally {
+        println("this is the final block")
+    }
+    println("You entered : $input")
 
-    val list = listOf(1,2,3,4,5)
-    val array = arrayOf(1,2,3,4,5)
-
-    println("List: $list")
-    printAlternately(array)
 
 }
 
